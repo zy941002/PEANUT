851ae52c3c34c157879523522d5530c6b7628f73
root <root@iZ28y0alu2tZ.(none)>
2016-08-01 17:31:21 +0800

    picPlayer

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 module.exports = function(app) {
   app.get('/', function (req, res) {
-    res.render('index', {});
+    res.render('index');
   });
   app.get('/Ball', function (req, res) {
   	res.sendfile('index')
@@ -16,6 +16,21 @@ module.exports = function(app) {
     res.sendfile('index')
   });
   app.get('/blink', function (req, res) {
+    res.sendfile('index')
+  });
+  app.get('/loading', function (req, res) {
+    res.sendfile('index')
+  });
+  app.get('/lineBall', function (req, res) {
+    res.sendfile('index')
+  });
+  app.get('/shake', function (req, res) {
+    res.sendfile('index')
+  });
+  app.get('/login', function (req, res) {
+    res.sendfile('index')
+  });
+  app.get('/picPlayer', function (req, res) {
     res.sendfile('index')
   });
 };
